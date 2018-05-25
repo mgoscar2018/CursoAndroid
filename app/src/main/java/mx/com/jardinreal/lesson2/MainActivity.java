7c8bdb8a41fc8103faa914b3b6914d37eff0e5de
mgoscar2018 <39196914+mgoscar2018@users.noreply.github.com>
2018-05-25 13:11:50 -0500

    Se agregó mensaje temporal tipo pop up

--- app/src/main/java/mx/com/jardinreal/lesson2/MainActivity.java
+++ app/src/main/java/mx/com/jardinreal/lesson2/MainActivity.java
@@ -2,8 +2,11 @@ package mx.com.jardinreal.lesson2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,31 +39,40 @@ public class MainActivity extends AppCompatActivity {
      */
     public void realizarOrden(View view) {
         //mostrarPrecio(5 * cantidad);
-        String precioMensaje = "Total: $"+(5*cantidad)+"\n¡Gracias!";
-        mostrarMensaje(precioMensaje);
+        int precio = calcularPrecio();
+        mostrarMensaje(crearResumenOrden(precio));
+    }
+
+    /**
+     * Calculates the price of the order.
+     * @return precio total de la venta
+     */
+    private int calcularPrecio() {
+        return cantidad * 5;
+    }
+
+    /*
+    * Arma el resumen de la orden
+     */
+    private String crearResumenOrden (int precio) {
+        Toast.makeText(getApplicationContext(), "¡Muchas gracias por tu compra!", Toast.LENGTH_SHORT).show(); //Muestra un mensaje temporal en pantalla
+        return "Nombre: Oscar Miranda\n"+"Cantidad: "+cantidad+"\nTotal: $"+precio+"\n¡Gracias!";
     }
 
     /**
      * Este método muestra en pantalla el valor que reciba como "numero"
+     * @param numero es la cantidad a desplegar en pantalla
      */
     private void display(int numero) {
         TextView quantityTextView = (TextView) findViewById(R.id.cantidad_text_view);
         quantityTextView.setText("" + numero);
     }
 
-    /**
-     * Este método despliega en pantalla el precio indicado
-     */
-    private void mostrarPrecio(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.precio_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
+     /**
      * Este método despliega el texto que envíen dentro del contenedor de precio
      */
     private void mostrarMensaje(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.precio_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.resumen_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
